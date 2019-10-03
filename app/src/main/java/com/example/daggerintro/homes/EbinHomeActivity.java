c4d93e22b2c770d1d168e13fdd944dabd03e37b9
Ebin Joy <ebinjoy999@gmail.com>
2019-10-03 14:31:18 +0530

    Chicken Fried rice @ name change

--- app/src/main/java/com/example/daggerintro/homes/EbinHomeActivity.java
+++ app/src/main/java/com/example/daggerintro/homes/EbinHomeActivity.java
@@ -22,7 +22,7 @@ public class EbinHomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ebin_home);
         setupNextActivity();
 
         SwiggyBoyComponent swiggyBoyComponent =  DaggerSwiggyBoyComponent.builder().build();
